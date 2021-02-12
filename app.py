81a358363e45e7207f54c11f9fa3de985f492b28
RyanCrasta <65001186+RyanCrasta@users.noreply.github.com>
2021-02-12 15:54:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, Response
+from camera import VideoCamera
+from collections import defaultdict
+from imutils.video import VideoStream
+app =  Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+def gen(camera):
+	(model, face_detector, open_eyes_detector,left_eye_detector,right_eye_detector, video_capture, images) = camera.init()
+	data = camera.process_and_encode(images)
+	eyes_detected = defaultdict(str)
+	while True:
+		frame = camera.detect_and_display(model, video_capture, face_detector, open_eyes_detector,left_eye_detector,right_eye_detector, data, eyes_detected)
+		#frame = camera.get_frame()
+		yield(b'--frame\r\n'
+			b'Content-Type: image/jpeg\r\n\r\n' + frame
+			+ b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+	return Response(gen(VideoCamera()),
+		mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+	app.run(host = '0.0.0.0', port='5000', debug=True)
