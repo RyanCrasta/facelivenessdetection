aadbc8c6f04ce43a007775a1bc30a1bc7feab9fe
RyanCrasta <65001186+RyanCrasta@users.noreply.github.com>
2021-02-13 14:18:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,5 +27,6 @@ def video_feed():
 
 
 if __name__ == '__main__':
+	port = int(os.environ.get("PORT", 5000))
 	#port = int(os.environ.get("PORT", 33507))
-	app.run(debug=True)
+	app.run(host='0.0.0.0', port=port)
